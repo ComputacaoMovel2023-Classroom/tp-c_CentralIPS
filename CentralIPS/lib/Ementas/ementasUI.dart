c75fe4bb212d2140606b3efe0de05b07b84b6701
Carlos-Luz <202100311@estudantes.ips.pt>
2023-04-28 15:59:00 +0100

    ementas (falta plano semanal)

--- CentralIPS/lib/Ementas/ementasUI.dart
+++ CentralIPS/lib/Ementas/ementasUI.dart
@@ -1,7 +1,4 @@
-// ignore: duplicate_ignore
-// ignore: file_names
-// ignore_for_file: file_names
-
+import 'package:centralips/Ementas/ButtonsUI.dart';
 import 'package:flutter/material.dart';
 
 class EmentasUI extends StatelessWidget {
@@ -9,28 +6,61 @@ class EmentasUI extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        leading: const Icon(Icons.menu),
-      ),
-      body: Container(
-        decoration: BoxDecoration(
-          color: Colors.blueGrey[100],
-          borderRadius: BorderRadius.circular(30),
-        ),
-        child: const Align(
-          alignment: Alignment.topLeft,
-          child: Padding(
-            padding: EdgeInsets.all(20),
-            child: Text(
-              "Ementas",
-              style: TextStyle(fontSize: 24),
+    return Stack(
+      children: [
+        Container(
+          height: 200,
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/images/chairsips.png'),
+              fit: BoxFit.fitWidth,
             ),
           ),
         ),
-      ),
+        Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+            leading: const Icon(Icons.menu),
+          ),
+          backgroundColor: Colors.transparent,
+          body: Container(
+            decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.vertical(
+                  top: Radius.circular(30),
+                )),
+            child: Align(
+              alignment: Alignment.topLeft,
+              child: Padding(
+                padding: const EdgeInsets.all(20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Text(
+                      "Ementas",
+                      style: TextStyle(
+                        fontSize: 35,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      "Horário de Funcionamento",
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    Text(
+                      "12h-15h",
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    ButtonUI(),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
