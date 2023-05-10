d8414ca217c739e2d76bdde50cafbad6060c93e0
pedromouraips <100920362+pedromouraips@users.noreply.github.com>
2023-05-10 11:56:14 +0100

    Firebase Realtime Database connection
    Recorrer ao singleton para ter acesso à instância por toda a app

---
+++ CentralIPS/lib/Database/CentralDB.dart
@@ -0,0 +1,19 @@
+import 'package:firebase_database/firebase_database.dart';
+
+class CentralDB {
+  static final CentralDB _singleton = CentralDB._internal();
+  DatabaseReference? _databaseReference;
+
+  factory CentralDB() {
+    return _singleton;
+  }
+
+  CentralDB._internal() {
+    // Initialize Firebase database instance
+    _databaseReference ??= FirebaseDatabase.instance.ref();
+  }
+
+  DatabaseReference? get databaseReference {
+    return _databaseReference;
+  }
+}
