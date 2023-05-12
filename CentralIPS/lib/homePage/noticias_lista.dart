41b305117856219459f5b4de0abb575423dbf2a4
pedromouraips <100920362+pedromouraips@users.noreply.github.com>
2023-05-12 23:33:31 +0100

    Carregar homepage destaques pelo servidor. Quase!

--- CentralIPS/lib/homePage/noticias_lista.dart
+++ CentralIPS/lib/homePage/noticias_lista.dart
@@ -1,36 +1,54 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 
 import 'noticias_lista_item.dart';
 
-class NoticiasLista extends StatelessWidget {
+class NoticiasLista extends StatefulWidget {
   const NoticiasLista({super.key});
 
   @override
+  _NoticiasListaState createState() => _NoticiasListaState();
+}
+
+class _NoticiasListaState extends State<NoticiasLista> {
+  List<Map<String, dynamic>> _noticiasData = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchData();
+  }
+
+  Future<void> _fetchData() async {
+    final user = FirebaseAuth.instance.currentUser;
+    FirebaseDatabase.instance.ref().child('noticias').onValue.listen((event) {
+      DataSnapshot snapshot = event.snapshot;
+
+      var userData = snapshot.value as Map;
+
+      setState(() {
+        _noticiasData = userData['noticias'] ?? [];
+      });
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return ListView(
-      scrollDirection: Axis.horizontal,
-      children: const [
-        NoticiasListaItem(
-          assetName: "assets/images/noticia1.png",
-          titletext: "Noticia 1\nExemplo de texto",
-        ),
-        NoticiasListaItem(
-          assetName: "assets/images/noticia2.png",
-          titletext: "Noticia 2\nExemplo de texto",
-        ),
-        NoticiasListaItem(
-          assetName: "assets/images/noticia3.png",
-          titletext: "Noticia 3\nExemplo de texto",
-        ),
-        NoticiasListaItem(
-          assetName: "assets/images/noticia1.png",
-          titletext: "Noticia 4\nExemplo de texto",
-        ),
-        NoticiasListaItem(
-          assetName: "assets/images/noticia2.png",
-          titletext: "Noticia 5\nExemplo de texto",
-        ),
-      ],
+    return Builder(
+      builder: (BuildContext context) {
+        return ListView(
+          scrollDirection: Axis.horizontal,
+          children: _noticiasData.map((noticiaData) {
+            return NoticiasListaItem(
+              assetName:
+                  noticiaData['assetName'] ?? 'assets/images/noticia1.png',
+              titletext:
+                  noticiaData['titletext'] ?? 'Noticia\nExemplo de texto',
+            );
+          }).toList(),
+        );
+      },
     );
   }
 }
