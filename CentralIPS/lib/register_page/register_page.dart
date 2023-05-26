1be0abaf1f412db17361aa316c3059958ecfacd3
RuiJesusIps <202100279@estudantes.ips.pt>
2023-05-26 13:52:33 +0100

    login e registro done
    verificações e email de verificação done

--- CentralIPS/lib/register_page/register_page.dart
+++ CentralIPS/lib/register_page/register_page.dart
@@ -1,9 +1,8 @@
-import 'package:centralips/Cubit/index_cubit.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_material_pickers/flutter_material_pickers.dart';
 import '../loginPage/login_page.dart';
 import 'package:intl/intl.dart';
 
@@ -45,7 +44,8 @@ Widget buildName() {
                   color: Colors.black26, blurRadius: 6, offset: Offset(0, 2))
             ]),
         height: 60,
-        child: TextField(
+        child: TextFormField(
+          validator: userNameValidator,
           controller: _nameController,
           keyboardType: TextInputType.name,
           style: const TextStyle(color: Colors.black87),
@@ -58,6 +58,13 @@ Widget buildName() {
       )
     ],
   );
+}
+
+String? userNameValidator(String? value) {
+  if (value == null || value.isEmpty) {
+    return 'Insira o seu nome';
+  }
+  return null; // Return null if the validation passes
 }
 
 Widget buildPhoto() {
@@ -80,7 +87,8 @@ Widget buildPhoto() {
                   color: Colors.black26, blurRadius: 6, offset: Offset(0, 2))
             ]),
         height: 60,
-        child: TextField(
+        child: TextFormField(
+          validator: userPhtolidator,
           controller: _photoController,
           keyboardType: TextInputType.name,
           style: const TextStyle(color: Colors.black87),
@@ -93,6 +101,19 @@ Widget buildPhoto() {
       )
     ],
   );
+}
+
+String? userPhtolidator(String? value) {
+  if (value == null || value.isEmpty) {
+    return 'Insira o url do seu avatar';
+  } //check if is an url or not
+  else if ( //check if is a valid url
+      !Uri.parse(value).isAbsolute ||
+          !value.startsWith('http://') && !value.startsWith('https://') ||
+          value.contains(' ')) {
+    return 'Insira um url válido';
+  }
+  return null; // Return null if the validation passes
 }
 
 Widget buildBirthdate(BuildContext context) {
@@ -130,7 +151,8 @@ Widget buildBirthdate(BuildContext context) {
             }
           },
           child: AbsorbPointer(
-            child: TextField(
+            child: TextFormField(
+              validator: birthDateValidator,
               controller: _birthdateController,
               keyboardType: TextInputType.datetime,
               style: const TextStyle(color: Colors.black87),
@@ -145,6 +167,18 @@ Widget buildBirthdate(BuildContext context) {
       )
     ],
   );
+}
+
+String? birthDateValidator(String? value) {
+  if (value == null || value.isEmpty) {
+    return 'Insira uma data';
+  }
+  final currentDate = DateTime.now();
+  final enteredDate = DateTime.parse(value);
+  if (!enteredDate.isBefore(currentDate)) {
+    return 'Data não pode ser no futuro';
+  }
+  return null;
 }
 
 Widget buildEmail() {
@@ -167,7 +201,8 @@ Widget buildEmail() {
                   color: Colors.black26, blurRadius: 6, offset: Offset(0, 2))
             ]),
         height: 60,
-        child: TextField(
+        child: TextFormField(
+          validator: emailValidator,
           controller: _emailController,
           keyboardType: TextInputType.emailAddress,
           style: const TextStyle(color: Colors.black87),
@@ -182,7 +217,16 @@ Widget buildEmail() {
   );
 }
 
-Widget buildGender() {
+String? emailValidator(String? value) {
+  if (value == null || value.isEmpty) {
+    return 'Insira um email';
+  } else if (!value.contains('@')) {
+    return 'Insira um email válido';
+  }
+  return null;
+}
+
+Widget buildGender(BuildContext context) {
   return Column(
     crossAxisAlignment: CrossAxisAlignment.start,
     children: <Widget>[
@@ -202,22 +246,43 @@ Widget buildGender() {
                   color: Colors.black26, blurRadius: 6, offset: Offset(0, 2))
             ]),
         height: 60,
-        child: TextField(
-          controller: _genderController,
-          keyboardType: TextInputType.name,
-          style: const TextStyle(color: Colors.black87),
-          decoration: const InputDecoration(
-              border: InputBorder.none,
-              contentPadding: EdgeInsets.only(top: 6, left: 10),
-              hintText: 'Escolha o género',
-              hintStyle: TextStyle(color: Colors.black38)),
+        child: GestureDetector(
+          onTap: () async {
+            showMaterialScrollPicker<String>(
+              context: context,
+              title: 'Escolha o género',
+              items: ["Masculino", "Feminino", "Outro"],
+              selectedItem: _genderController.text,
+              onChanged: (value) => _genderController.text = value,
+            );
+          },
+          child: AbsorbPointer(
+            child: TextFormField(
+              validator: genderValidator,
+              controller: _genderController,
+              keyboardType: TextInputType.name,
+              style: const TextStyle(color: Colors.black87),
+              decoration: const InputDecoration(
+                  border: InputBorder.none,
+                  contentPadding: EdgeInsets.only(top: 6, left: 10),
+                  hintText: 'Escolha o género',
+                  hintStyle: TextStyle(color: Colors.black38)),
+            ),
+          ),
         ),
       )
     ],
   );
 }
 
-Widget buildRole() {
+String? genderValidator(String? value) {
+  if (value == null || value.isEmpty) {
+    return 'Insira um género';
+  }
+  return null;
+}
+
+Widget buildRole(BuildContext context) {
   return Column(
     crossAxisAlignment: CrossAxisAlignment.start,
     children: <Widget>[
@@ -237,19 +302,40 @@ Widget buildRole() {
                   color: Colors.black26, blurRadius: 6, offset: Offset(0, 2))
             ]),
         height: 60,
-        child: TextField(
-          controller: _roleController,
-          keyboardType: TextInputType.name,
-          style: const TextStyle(color: Colors.black87),
-          decoration: const InputDecoration(
-              border: InputBorder.none,
-              contentPadding: EdgeInsets.only(top: 6, left: 10),
-              hintText: 'A sua função',
-              hintStyle: TextStyle(color: Colors.black38)),
+        child: GestureDetector(
+          onTap: () async {
+            showMaterialScrollPicker<String>(
+              context: context,
+              title: 'Escolha a sua função',
+              items: ["Estudante", "Docente", "Funcionário"],
+              selectedItem: _roleController.text,
+              onChanged: (value) => _roleController.text = value,
+            );
+          },
+          child: AbsorbPointer(
+            child: TextFormField(
+              validator: roleValidator,
+              controller: _roleController,
+              keyboardType: TextInputType.name,
+              style: const TextStyle(color: Colors.black87),
+              decoration: const InputDecoration(
+                  border: InputBorder.none,
+                  contentPadding: EdgeInsets.only(top: 6, left: 10),
+                  hintText: 'A sua função',
+                  hintStyle: TextStyle(color: Colors.black38)),
+            ),
+          ),
         ),
       )
     ],
   );
+}
+
+String? roleValidator(String? value) {
+  if (value == null || value.isEmpty) {
+    return 'Introduza a sua função';
+  }
+  return null;
 }
 
 Widget buildNumber() {
@@ -272,7 +358,8 @@ Widget buildNumber() {
                   color: Colors.black26, blurRadius: 6, offset: Offset(0, 2))
             ]),
         height: 60,
-        child: TextField(
+        child: TextFormField(
+          validator: numberValidator,
           controller: _numberController,
           keyboardType: TextInputType.number,
           style: const TextStyle(color: Colors.black87),
@@ -285,6 +372,15 @@ Widget buildNumber() {
       )
     ],
   );
+}
+
+String? numberValidator(String? value) {
+  if (value == null || value.isEmpty) {
+    return 'Insira o seu número';
+  } else if (value.length != 9) {
+    return 'Insira um número válido';
+  }
+  return null;
 }
 
 Widget buildPassword() {
@@ -307,7 +403,8 @@ Widget buildPassword() {
                   color: Colors.black26, blurRadius: 6, offset: Offset(0, 2))
             ]),
         height: 60,
-        child: TextField(
+        child: TextFormField(
+          validator: passwordValidator,
           controller: _passwordController,
           obscureText: true,
           style: const TextStyle(color: Colors.black87),
@@ -322,62 +419,114 @@ Widget buildPassword() {
   );
 }
 
-Widget buildRegisterBtn(BuildContext context) {
+String? passwordValidator(String? value) {
+  if (value == null || value.isEmpty) {
+    return 'Insira a sua password';
+  }
+  return null;
+}
+
+Widget buildRegisterBtn(BuildContext context, GlobalKey<FormState> formKey) {
   return Container(
     padding: const EdgeInsets.symmetric(vertical: 25),
     width: double.infinity,
     child: ElevatedButton(
       onPressed: () async {
-        try {
-          final UserCredential userCredential =
-              await auth.createUserWithEmailAndPassword(
-            email: _emailController.text,
-            password: _passwordController.text,
+        if (formKey.currentState!.validate()) {
+          try {
+            final UserCredential userCredential =
+                await auth.createUserWithEmailAndPassword(
+              email: _emailController.text,
+              password: _passwordController.text,
+            );
+            final userRef = FirebaseDatabase.instance
+                .ref()
+                .child('users')
+                .child(userCredential.user!.uid);
+
+            userCredential.user?.updatePhotoURL(_photoController.text);
+            userCredential.user?.updateDisplayName(_nameController.text);
+
+            await userRef.set({
+              'name': _nameController.text,
+              'photo': _photoController.text,
+              'number': _numberController.text,
+              'birthdate': _birthdateController.text,
+              'gender': _genderController.text,
+              'role': _roleController.text,
+            });
+
+            //create ipshealth user
+            final ipshealthUserRef = FirebaseDatabase.instance
+                .ref('ipshealth/${userCredential.user?.uid}');
+
+            //set a data of today
+            final DateTime now = DateTime.now();
+            final DateFormat formatter = DateFormat('yyyy-MM-dd');
+            final String formatted = formatter.format(now);
+
+            await ipshealthUserRef.set({formatted: 0});
+
+            // Imprima o UID do usuário recém-criado
+            print(userCredential.user?.uid);
+
+            //if is all valid send a verification email
+            await userCredential.user?.sendEmailVerification();
+
+            //show a dialog to confirm the email
+          } on FirebaseAuthException catch (e) {
+            if (e.code == 'weak-password') {
+              showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    title: const Text('Erro'),
+                    content: const Text('A senha fornecida é muito fraca.'),
+                    actions: [
+                      TextButton(
+                        child: const Text('OK'),
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                      ),
+                    ],
+                  );
+                },
+              );
+            } else if (e.code == 'email-already-in-use') {
+              showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    title: const Text('Erro'),
+                    content: const Text(
+                        'O e-mail fornecido já está em uso por outra conta.'),
+                    actions: [
+                      TextButton(
+                        child: const Text('OK'),
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                      ),
+                    ],
+                  );
+                },
+              );
+            }
+          } catch (e) {
+            print(e);
+          }
+          // If the form is valid, display a Snackbar.
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text('Email De verificação enviado!')),
           );
-          final userRef = FirebaseDatabase.instance
-              .ref()
-              .child('users')
-              .child(userCredential.user!.uid);
 
-          userCredential.user?.updatePhotoURL(_photoController.text);
-          userCredential.user?.updateDisplayName(_nameController.text);
-
-          await userRef.set({
-            'name': _nameController.text,
-            'photo': _photoController.text,
-            'number': _numberController.text,
-            'birthdate': _birthdateController.text,
-            'gender': _genderController.text,
-            'role': _roleController.text,
-          });
-
-          //create ipshealth user
-          final ipshealthUserRef = FirebaseDatabase.instance
-              .ref('ipshealth/${userCredential.user?.uid}');
-
-          //set a data of today
-          final DateTime now = DateTime.now();
-          final DateFormat formatter = DateFormat('yyyy-MM-dd');
-          final String formatted = formatter.format(now);
-
-          await ipshealthUserRef.set({formatted: 0});
-
-          // Imprima o UID do usuário recém-criado
-          print(userCredential.user?.uid);
-
-          context.read<FooterMenuCubit>().selectItem(2);
           Navigator.push(
             context,
-            MaterialPageRoute(builder: (context) => const LoginPage()),
+            MaterialPageRoute(builder: (_) => const LoginPage()),
           );
-        } on FirebaseAuthException catch (e) {
-          if (e.code == 'weak-password') {
-            print('A senha fornecida é muito fraca.');
-          } else if (e.code == 'email-already-in-use') {
-            print('O e-mail fornecido já está em uso por outra conta.');
-          }
-        } catch (e) {
-          print(e);
+        } else {
+          print('not valid');
         }
       },
       style: ButtonStyle(
@@ -426,6 +575,7 @@ Widget buildLoginBtn(BuildContext context) {
 }
 
 class _RegisterPageState extends State<RegisterPage> {
+  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     DateTime birthdate;
@@ -462,26 +612,30 @@ class _RegisterPageState extends State<RegisterPage> {
                               fontSize: 17,
                               fontWeight: FontWeight.normal),
                         ),
-                        const SizedBox(height: 60),
-                        buildName(),
-                        const SizedBox(height: 15),
-                        buildEmail(),
-                        const SizedBox(height: 15),
-                        buildBirthdate(context),
-                        const SizedBox(height: 15),
-                        buildGender(),
-                        const SizedBox(height: 15),
-                        buildRole(),
-                        const SizedBox(height: 15),
-                        buildNumber(),
-                        const SizedBox(height: 15),
-                        buildPassword(),
-                        const SizedBox(height: 15),
-                        buildPhoto(),
-                        const SizedBox(height: 15),
-                        buildRegisterBtn(context),
-                        const SizedBox(height: 5),
-                        buildLoginBtn(context),
+                        Form(
+                            key: _formKey,
+                            child: Column(children: <Widget>[
+                              const SizedBox(height: 60),
+                              buildName(),
+                              const SizedBox(height: 15),
+                              buildEmail(),
+                              const SizedBox(height: 15),
+                              buildBirthdate(context),
+                              const SizedBox(height: 15),
+                              buildGender(context),
+                              const SizedBox(height: 15),
+                              buildRole(context),
+                              const SizedBox(height: 15),
+                              buildNumber(),
+                              const SizedBox(height: 15),
+                              buildPassword(),
+                              const SizedBox(height: 15),
+                              buildPhoto(),
+                              const SizedBox(height: 15),
+                              buildRegisterBtn(context, _formKey),
+                              const SizedBox(height: 5),
+                              buildLoginBtn(context),
+                            ])),
                       ],
                     ),
                   ),
