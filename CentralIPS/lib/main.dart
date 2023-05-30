afedc8a54c998401955bf230652ff2c4b43fe6ad
pedromouraips <100920362+pedromouraips@users.noreply.github.com>
2023-05-30 15:26:48 +0100

    Only portrait mode - :lock:

--- CentralIPS/lib/main.dart
+++ CentralIPS/lib/main.dart
@@ -1,12 +1,9 @@
 import 'package:centralips/Bloc/libraryFilters/library_filters_bloc.dart';
-import 'package:centralips/homePage/home_page_ui.dart';
-import 'package:centralips/register_page/register_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:centralips/WelcomeScreen/welcome_screen.dart';
-import 'Sidebar/NavBar.dart';
-import 'Cubit/index_cubit.dart';
+import 'package:flutter/services.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -14,6 +11,12 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  WidgetsFlutterBinding.ensureInitialized();
+  // Step 3
+  SystemChrome.setPreferredOrientations(
+          [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown])
+      .then((value) => runApp(const MyAppDemo()));
   runApp(const MyAppDemo());
 }
 
@@ -23,15 +26,14 @@ class MyAppDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-        create: (context) => LibraryFiltersBloc()
-        ..add(LibraryFilterLoad()),
+        create: (context) => LibraryFiltersBloc()..add(LibraryFilterLoad()),
         child: MaterialApp(
           title: 'CentralIPS',
           theme: ThemeData(
             primarySwatch: Colors.blue,
             useMaterial3: true,
           ),
-          home: WelcomeScreen(),
+          home: const WelcomeScreen(),
         ));
   }
 }
