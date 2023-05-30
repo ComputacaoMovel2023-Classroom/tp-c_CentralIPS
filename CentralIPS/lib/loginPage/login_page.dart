f37f3b63dcdcf972d42f3902ba5ecd9b5d02af38
pedromouraips <100920362+pedromouraips@users.noreply.github.com>
2023-05-30 16:21:52 +0100

    Progress indicator Login

--- CentralIPS/lib/loginPage/login_page.dart
+++ CentralIPS/lib/loginPage/login_page.dart
@@ -1,5 +1,6 @@
 import 'package:centralips/Cubit/index_cubit.dart';
 import 'package:centralips/homePage/home_page_ui.dart';
+import 'package:centralips/progress.dart';
 import 'package:centralips/register_page/register_page_provider.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
@@ -97,6 +98,7 @@ Widget buildLoginBtn(BuildContext context) {
     width: double.infinity,
     child: ElevatedButton(
       onPressed: () async {
+        LoadingIndicatorDialog().show(context);
         try {
           //sing out from google
           // await GoogleSignIn().signOut();
