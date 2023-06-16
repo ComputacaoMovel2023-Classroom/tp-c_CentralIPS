dc52228db19e823b627214f0896a57406d5b4286
pedromouraips <100920362+pedromouraips@users.noreply.github.com>
2023-06-16 11:20:42 +0100

    Commit david
    Co-Authored-By: DavidGanhao <101137382+DavidGanhao@users.noreply.github.com>

--- CentralIPS/lib/Departamentos/departmentFavorite.dart
+++ CentralIPS/lib/Departamentos/departmentFavorite.dart
@@ -1,62 +1,22 @@
-import 'package:centralips/Departamentos/department.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 
-class DepartmentFavorite extends StatefulWidget {
-  Department department;
-  double size = 26;
+class DepartmentFavorite extends StatelessWidget {
+  bool greyColor = false;
+  double size = 0;
 
-  DepartmentFavorite(this.department, this.size, {super.key});
+  DepartmentFavorite(this.greyColor, this.size, {super.key});
 
-  @override
-  State<DepartmentFavorite> createState() => DepartmentFavoriteState();
-}
-
-class DepartmentFavoriteState extends State<DepartmentFavorite> {
-  final FirebaseAuth auth = FirebaseAuth.instance;
-  bool greyColor = true;
-
-  @override
-  void initState() {
-    super.initState();
-    final user = auth.currentUser;
-    greyColor = !widget.inicialState;
-  }
-
-  void inputData() {
-    final user = auth.currentUser;
-
-    if (widget.department.usersId.contains(user!.uid)) {
-      widget.department.usersId.remove(user.uid);
-    } else {
-      widget.department.usersId.add(user.uid);
-    }
-
-    DatabaseReference vlRef =
-        FirebaseDatabase.instance.ref().child("Departamentos");
-    vlRef
-        .child(widget.department.id)
-        .update({'usersId': widget.department.usersId});
+  void changeGreyColor() {
+    greyColor = !greyColor;
   }
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: widget.size,
-      width: widget.size,
-      child: IconButton(
-        padding: const EdgeInsets.all(0),
-        iconSize: widget.size,
-        icon: Icon(Icons.favorite,
-            color: greyColor ? Colors.grey : Colors.red[300]),
-        onPressed: () {
-          inputData();
-          setState(() {
-            greyColor = !greyColor;
-          });
-        },
-      ),
+      height: size,
+      width: size,
+      child: Icon(Icons.favorite,
+          color: greyColor ? Colors.grey : Colors.red[300]),
     );
   }
 }
