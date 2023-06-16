b2290f5edde18fce2ff37f1fc6a6c9088b0ff9a4
202100270AC <101176327+202100270AC@users.noreply.github.com>
2023-06-16 12:12:00 +0100

    Update profile.dart

--- CentralIPS/lib/Profile/profile.dart
+++ CentralIPS/lib/Profile/profile.dart
@@ -6,6 +6,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 
+///Widget that represents the user profile
 class Profile extends StatefulWidget {
   const Profile({Key? key}) : super(key: key);
 
